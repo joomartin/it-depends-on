e01f9a10e26ab14bb4f4517e754e92125ba520eb
Joó Martin <m4rt1n.j00@gmail.com>
2018-09-10 15:27:53 +0200

    Fix bug and clean up

--- src/DependencyParser/PhpDependencyParser.php
+++ src/DependencyParser/PhpDependencyParser.php
@@ -26,7 +26,7 @@ class PhpDependencyParser
 
     public function __construct(PhpParserAdapter $phpParser, NodeFinderAdapter $nodeFinder)
     {
-        $this->phpParser = $phpParser;    
+        $this->phpParser = $phpParser;
         $this->nodeFinder = $nodeFinder;
         $this->nodeDumper = new NodeDumper;
 
@@ -35,14 +35,14 @@ class PhpDependencyParser
          */
     }
 
-    public function parse(string $code): DependencyGroup
+    public function parse(string $code) : DependencyGroup
     {
         $ast = $this->phpParser->parse($code);
 
         return $this->getAllDependency($ast);
     }
 
-    protected function getAllDependency(array $ast): DependencyGroup
+    protected function getAllDependency(array $ast) : DependencyGroup
     {
         $inline = $this->getInlineDependencies($ast);
         $injected = $this->getInjectedDependencies($ast);
@@ -53,7 +53,7 @@ class PhpDependencyParser
     /**
      * @return Dependency[]
      */
-    protected function getInjectedDependencies(array $ast): array
+    protected function getInjectedDependencies(array $ast) : array
     {
         $methods = $this->getMethodsWithInjectedDependencies($ast);
         $deps = $this->getUniqueDependenciesByMethods($methods, self::TYPE_INJECTED);
@@ -65,44 +65,43 @@ class PhpDependencyParser
     /**
      * @return Dependency[]
      */
-    protected function getInlineDependencies(array $ast): array
+    protected function getInlineDependencies(array $ast) : array
     {
         $methods = $this->getMethodsWithInlineDependencies($ast);
         $deps = $this->getUniqueDependenciesByMethods($methods, self::TYPE_INLINE);
         $this->setResolvedClassNames($ast, $deps);
 
         return $deps;
-    }    
+    }
 
     /**
      * @param Dependency[] $dependencies
      */
     protected function setResolvedClassNames(array $ast, array $dependencies)
-    {   
+    {
         foreach ($dependencies as $dep)
-            $dep->fqcn = $this->resolveClassName($ast, $dep->name);
+            $dep->fqcn = $this->getResolvedClassName($ast, $dep->name);
     }
 
     /**
      * @return MethodDependency[]
      */
-    protected function getMethodsWithInjectedDependencies(array $ast): array
+    protected function getMethodsWithInjectedDependencies(array $ast) : array
     {
         return $this->parseMethods($ast, function ($classMethod) {
-            // @todo CLASS NAME RESOLVE
             $methodDependency = new MethodDependency($classMethod->name->name);
 
-            foreach ($classMethod->params as $param) {                            
+            foreach ($classMethod->params as $param) {
                 $name = $this->nodeFinder->findInstanceOf($param, NodeFinderAdapter::USE_NAME_INSTANCE);
 
                 // if no Node\Name found for this parameter, then it's a simple scalar type paramter
-                if (empty($name)) 
+                if (empty($name))
                     continue;
 
                 $methodDependency->addParts($name[0]->parts);
             }
 
-            if ($methodDependency->hasDependencies()) 
+            if ($methodDependency->hasDependencies())
                 return $methodDependency;
         });
     }
@@ -110,31 +109,29 @@ class PhpDependencyParser
     /**
      * @return MethodDependency[]
      */
-    protected function getMethodsWithInlineDependencies(array $ast): array
+    protected function getMethodsWithInlineDependencies(array $ast) : array
     {
         return $this->parseMethods($ast, function ($classMethod) {
-            // @todo CLASS NAME RESOLVE
             $methodDependency = new MethodDependency($classMethod->name->name);
+            $newExpressions = $this->nodeFinder->findInstanceOf($classMethod->stmts, NodeFinderAdapter::NEW_EXPR);
 
-            $newExpressions = $this->nodeFinder->findInstanceOf($classMethod->stmts, NodeFinderAdapter::NEW_EXPR);            
-
-            if (empty($newExpressions)) 
+            if (empty($newExpressions))
                 return;
 
             foreach ($newExpressions as $newExpr)
                 $methodDependency->addParts($newExpr->class->parts);
-            
-            return $methodDependency;            
+
+            return $methodDependency;
         });
     }
 
-    protected function parseMethods(array $ast, \Closure $fn): array
+    protected function parseMethods(array $ast, \Closure $fn) : array
     {
         return array_filter(
             array_map(function ($classMethod) use ($fn) {
                 return $fn($classMethod);
-            }, $this->nodeFinder->findInstanceOf($ast, NodeFinderAdapter::CLASS_METHOD_INSTANCE)), 
-            
+            }, $this->nodeFinder->findInstanceOf($ast, NodeFinderAdapter::CLASS_METHOD_INSTANCE)),
+
             function ($v) {
                 return $v !== null;
             }
@@ -144,18 +141,18 @@ class PhpDependencyParser
     /**
      * @return Dependency[]
      */
-    protected function getUniqueDependenciesByMethods(array $methodDependencies, string $type): array
+    protected function getUniqueDependenciesByMethods(array $methodDependencies, string $type) : array
     {
         $dependencies = [];
 
         foreach ($methodDependencies as $methodDep) {
             /** @var MethodDependency $methodDep */
             foreach ($methodDep->dependencies as $d) {
-                if (!array_key_exists($d, $dependencies)) 
+                if (!array_key_exists($d, $dependencies))
                     $dependencies[$d] = new Dependency($d, $type);
 
                 $dependencies[$d]->addMethod($methodDep->method);
-            }            
+            }
         }
 
         return $dependencies;
@@ -165,7 +162,7 @@ class PhpDependencyParser
      * Returns all class names that accours in use statements at the top of the class
      * @return string[]
      */
-    protected function getUsedDependencies(array $ast): array
+    protected function getUsedDependencies(array $ast) : array
     {
         $useUseStatements = $this->nodeFinder->findInstanceOf($ast, NodeFinderAdapter::USE_INSTANCE);
         $names = $this->nodeFinder->findInstanceOf($useUseStatements, NodeFinderAdapter::USE_NAME_INSTANCE);
@@ -175,7 +172,7 @@ class PhpDependencyParser
         }, $names);
     }
 
-    protected function resolveClassName(array $ast, string $className)
+    protected function getResolvedClassName(array $ast, string $className)
     {
         /**
          * @todo
@@ -185,11 +182,12 @@ class PhpDependencyParser
 
         $useStatements = $this->getUsedDependencies($ast);
         $items = array_filter($useStatements, function (string $fqcn) use ($className) {
-            return strripos($fqcn, $className, 0) === strlen($fqcn) - strlen($className);
+            $expectedPosition = strlen($fqcn) - strlen($className);
+            return strripos($fqcn, $className, 0) === $expectedPosition && $fqcn[$expectedPosition - 1] === '\\';
         });
 
         if (empty($items))
-            return $className;        
+            return $className;
 
         return array_values($items)[0];
     }
